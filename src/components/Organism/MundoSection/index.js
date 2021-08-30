768fff060de7b8ecb704f19b0036a7a4406602a9
Cirelly <cirellybryan@gmail.com>
2021-08-30 11:49:51 -0400

    mundo section responsive

--- src/components/Organism/MundoSection/index.js
+++ src/components/Organism/MundoSection/index.js
@@ -8,67 +8,82 @@ import faenadora from "../../../assets/icn_plantafaenadora.png";
 import produccion from "../../../assets/icn_produccionresponsable.png";
 import oficinaCentral from "../../../assets/icn_oficentral.png";
 
-
 const useStyles = makeStyles((theme) => ({
-  root: {   
-    paddingLeft: 100,
-    paddingRight: 100,
+  root: {
+    
   },
 }));
 
 const MundoSection = () => {
   const classes = useStyles();
   return (
-    <div id="mundo-as" className={classes.root}>
-      <Grid
-        container
-        spacing={0}
-        justifyContent="center"
-        direction="row"
-        alignItems="center"
+    <div id='mundo-as' className={classes.root}>
+      <Box
+        width='100%'
+        display='flex'
+        justifyContent='center'
+        alignItems='center'
       >
-        <Box m={3}>
-          <Typography variant="h3">
-            {" "}
-            Descubre nuestros procesos navegando en nuestro mundo{" "}
-          </Typography>
+        <Box width='80%' minWidth={300}>
+          <Grid
+            container
+            spacing={0}
+            justifyContent='center'
+            direction='row'
+            alignItems='center'
+          >
+            <Box m={3}>
+              <Typography align="center" variant='h3'>
+                Descubre nuestros procesos navegando en nuestro mundo
+              </Typography>
+            </Box>
+            <Grid container spacing={4} item xs={12}>
+              <Grid item lg={2} md={4} sm={6} xs={12}>
+                <NavCard image={historia} tittle='Historia' link='/historia' />
+              </Grid>
+              <Grid item lg={2} md={4} sm={6} xs={12}>
+                <NavCard
+                  image={oficinaCentral}
+                  tittle='Oficina '
+                  subtittle='Central'
+                  link='/oficina'
+                />
+              </Grid>
+              <Grid item lg={2} md={4} sm={6} xs={12}>
+                <NavCard
+                  image={crianza}
+                  tittle='Plantas '
+                  subtittle='Crianza'
+                  link='/granja'
+                />
+              </Grid>
+              <Grid item lg={2} md={4} sm={6} xs={12}>
+                <NavCard
+                  image={faenadora}
+                  tittle='Plantas '
+                  subtittle='Faenadora'
+                  link='/planta'
+                />
+              </Grid>
+              <Grid item lg={2} md={4} sm={6} xs={12}>
+                <NavCard
+                  image={produccion}
+                  tittle='Produccion Responsable'
+                  subtittle=''
+                  link='/produccion'
+                />
+              </Grid>
+              <Grid item lg={2} md={4} sm={6} xs={12}>
+                <NavCard
+                  image={comunidad}
+                  tittle='Comunidad'
+                  link='/comunidad'
+                />
+              </Grid>
+            </Grid>
+          </Grid>
         </Box>
-        <Grid container spacing={4} item xs={12}>
-          <Grid item lg={2} md={4} sm={6} xs={6}>
-            <NavCard image={historia} tittle="Historia" link="/historia"/>
-          </Grid>
-          <Grid item lg={2} md={4} sm={6} xs={6}>
-            <NavCard
-              image={oficinaCentral}
-              tittle="Oficina"
-              subtittle="Central"
-              link="/oficina"
-            />
-          </Grid>
-          <Grid item lg={2} md={4} sm={6} xs={6}>
-            <NavCard image={crianza} tittle="Plantas" subtittle="Crianza" link="/granja" />
-          </Grid>
-          <Grid item lg={2} md={4} sm={6} xs={6}>
-            <NavCard
-              image={faenadora}
-              tittle="Plantas "
-              subtittle="Faenadora"
-              link="/planta"
-            />
-          </Grid>
-          <Grid item lg={2} md={4} sm={6} xs={6}>
-            <NavCard
-              image={produccion}
-              tittle="Produccion"
-              subtittle="Responsable"
-              link="/produccion"
-            />
-          </Grid>
-          <Grid item lg={2} md={4} sm={6} xs={6}>
-            <NavCard image={comunidad} tittle="Comunidad" link="/comunidad" />
-          </Grid>
-        </Grid>
-      </Grid>
+      </Box>
     </div>
   );
 };
