d5e84b4e2e3689b22e16c9064f884e2b2c6741f6
gdoc7 <gabrielojeda7@gmail.com>
2021-08-31 00:28:07 -0400

    add evolution logos

--- src/components/Screens/Historia/index.js
+++ src/components/Screens/Historia/index.js
@@ -3,26 +3,22 @@ import ReactPlayer from "react-player";
 import timelineIcon from "../../../assets/enlace_lineadetiempo.png";
 import { Box, Grid, makeStyles } from "@material-ui/core";
 import { Link as RouterLink } from "react-router-dom";
-import flecha from "../../../assets/flecha.png"
-import { position } from "dom-helpers";
+import flecha from "../../../assets/flecha.png";
+import logoEvolucion from "../../../assets/logos_evo-desktop.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    paddingTop: 60,
+    
     height: "auto",
   },
 
-
-  video:{
-    borderRadius:20,
+  video: {
+    borderRadius: 20,
     overflow: "hidden",
-
   },
-  flecha:{
-    zIndex:-1,
-    position:"absolute"
-
- 
+  flecha: {
+    zIndex: -1,
+    position: "absolute",
   },
 
   gridContainer: {
@@ -38,49 +34,64 @@ const Historia = () => {
   const classes = useStyles();
   return (
     <div id="historia" className={classes.root}>
-        
+      <Grid container 
+        spacing={0}
+        justifyContent="center"
+        alignItems="center"> 
+        <Box
+          component="img"
+          width="50%"
+          src={logoEvolucion}
+          alt="Evolucion del logo"
+        />
+      </Grid>
 
-
-
-          <Grid container direction="row" spacing={0} justifyContent="center" alignItems="center">
+      <Grid
+        container
+        direction="row"
+        spacing={0}
+        justifyContent="center"
+        alignItems="center"
+      >
           <Box
-                  component="img"
-                  width="98%"
-                  height="18%"
-                  src={flecha}
-                  alt="flecha "
-                  className={classes.flecha}
-                />
+          component="img"
+          width="98%"
+          height="18%"
+          src={flecha}
+          alt="flecha "
+          className={classes.flecha}
+        />
+  
 
 
-            <Grid item xs={8} md={5}>
-             
-                  <ReactPlayer url="https://youtu.be/9PCbia4Rp_c" width="750px" height="500px" className={classes.video} />
-                
-              </Grid>
+        <Grid item xs={8} md={5}>
+          <ReactPlayer
+            url="https://youtu.be/9PCbia4Rp_c"
+            width="750px"
+            height="500px"
+            className={classes.video}
+          />
+        </Grid>
 
-
-
-              <Grid
-              item
-              xs={4} md={5}
-              container 
-              justifyContent="center" alignItems="center"
-            
-            >
-              <RouterLink to="/historia/timeline">
-                <Box
-                  component="img"
-                  width="300px"
-                  ml={10}
-                  src={timelineIcon}
-                  alt="timeline "
-                />
-              </RouterLink>
-            </Grid>
-            </Grid>
-
-
+        <Grid
+          item
+          xs={4}
+          md={5}
+          container
+          justifyContent="center"
+          alignItems="center"
+        >
+          <RouterLink to="/historia/timeline">
+            <Box
+              component="img"
+              width="300px"
+              ml={10}
+              src={timelineIcon}
+              alt="timeline "
+            />
+          </RouterLink>
+        </Grid>
+      </Grid>
     </div>
   );
 };
